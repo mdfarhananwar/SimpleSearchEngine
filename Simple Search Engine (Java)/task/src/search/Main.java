845b7075d0a050c9a88e5a8613231305035bc00c
Md Farhan Anwar <mdfarhananwar@gmail.com>
2024-02-08 01:12:12 +0530

    Added functionality to read text files

--- Simple Search Engine (Java)/task/src/search/Main.java
+++ Simple Search Engine (Java)/task/src/search/Main.java
@@ -1,5 +1,10 @@
 package search;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.io.File;
+
 
 public class Main {
     private static final int COMMAND_EXIT = 0;
@@ -8,11 +13,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] peopleDetails = inputPeopleDetails(scanner);
+        List<String> peopleDetails = inputPeopleDetails(scanner, args);
         printMenu();
         menu(scanner, peopleDetails);
     }
-    public static void menu(Scanner scanner, String[] peopleDetails) {
+    public static void menu(Scanner scanner, List<String> peopleDetails) {
         while(true) {
             int command = -1;
             try {
@@ -33,36 +38,52 @@ public class Main {
         }
     }
 
-    public static String[] inputPeopleDetails(Scanner scanner) {
-        System.out.println("Enter the number of people:");
-        int numberOfPeople = Integer.parseInt(scanner.nextLine());
-        String[] peopleDetails = new String[numberOfPeople];
-        System.out.println("Enter all people:");
-        for (int i = 0; i < numberOfPeople; i++) {
-            peopleDetails[i] = scanner.nextLine();
+    public static List<String> inputPeopleDetails(Scanner scanner, String[] args) {
+        List<String> peopleDetails = new ArrayList<>();
+        if (args[0].equals("--data")) {
+            String filePath = args[1]; // Provide the path to your text file
+            try (Scanner scannerFile = new Scanner(new File(filePath))) {
+                while (scannerFile.hasNextLine()) {
+                    String line = scannerFile.nextLine();
+                    // Process each line here
+                    peopleDetails.add(line);
+                    //System.out.println(line);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("Fail");
+            }
+        } else {
+
+            System.out.println("Enter the number of people:");
+            int numberOfPeople = Integer.parseInt(scanner.nextLine());
+            System.out.println("Enter all people:");
+            for (int i = 0; i < numberOfPeople; i++) {
+
+                peopleDetails.add(scanner.nextLine());
+            }
         }
         //searchQuery(scanner, peopleDetails);
         return peopleDetails;
     }
 
-    public static void searchQuery(Scanner scanner, String[] peopleDetails) {
+    public static void searchQuery(Scanner scanner, List<String> peopleDetails) {
         System.out.println("Enter a name or email to search all suitable people.");
-            boolean isPersonFound = false;
-            String query = scanner.nextLine().trim();
-            for (String people : peopleDetails) {
-                if(people.toLowerCase().contains(query.toLowerCase())) {
-                    if(!isPersonFound) {
-                        isPersonFound = true;
-                    }
-                    System.out.println(people);
+        boolean isPersonFound = false;
+        String query = scanner.nextLine().trim();
+        for (String people : peopleDetails) {
+            if(people.toLowerCase().contains(query.toLowerCase())) {
+                if(!isPersonFound) {
+                    isPersonFound = true;
                 }
-            if (!isPersonFound) {
-                System.out.println("No matching people found.");
+                System.out.println(people);
             }
+        }
+        if (!isPersonFound) {
+            System.out.println("No matching people found.");
         }
     }
 
-    public static void printAllPeople(String[] peopleDetails) {
+    public static void printAllPeople(List<String> peopleDetails) {
         System.out.println("=== List of people ===");
         for (String person : peopleDetails) {
             System.out.println(person);
